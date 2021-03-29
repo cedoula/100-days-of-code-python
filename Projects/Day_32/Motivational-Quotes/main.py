cd9cbc8f526e2486f12bf4ec6770763d5a713952
cedoula <cedric.lutonda@gmail.com>
2021-03-29 13:46:42 -0500

    Day 32 completed.

--- Projects/Day_32/Motivational-Quotes/main.py
+++ Projects/Day_32/Motivational-Quotes/main.py
@@ -1,18 +1,3 @@
-# import smtplib
-
-# my_email = "cedric.vanza@gmail.com"
-# password = "4RJM)v56RU1%rG"
-
-# with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-#     #Encrypt email
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(
-#         from_addr=my_email, 
-#         to_addrs="cedric.vanza@yahoo.com", 
-#         msg="Subject:Hello\n\nThis is the body of my email."
-#     )
-
 import datetime as dt
 from random import choice
 import smtplib
