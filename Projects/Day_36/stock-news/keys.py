92a29790dda07aea5fb4ac7e8e8c361bbb053159
cedoula <cedric.lutonda@gmail.com>
2021-04-02 22:25:43 -0500

    Day 36 completed.

---
+++ Projects/Day_36/stock-news/keys.py
@@ -0,0 +1,5 @@
+alpha_api_key = "4OD6G4ADODSA18VR"
+newsapi_api_key = "69f0854e91844fe0bd37a19c184e7ea1"
+#Twilio
+account_sid = "ACb65d511e1ddb55f81f6b6c2121dd6d9f"
+auth_token = "52b174a32d390976c94cc07a0a28dc1a"
