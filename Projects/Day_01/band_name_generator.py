e126333dfa8d919b3ad2afe7bffb94d5bca374af
removedporn <86824510+removedporn@users.noreply.github.com>
2022-10-12 20:15:58 +0800

    Delete Projects directory

--- Projects/Day_01/band_name_generator.py
+++
@@ -1,8 +0,0 @@
-# Greeting for the program.
-print("Welcome to the Band Name Generator App.")
-#Ask the user for the city that they grew up in.
-city = input("What city did you grow up in ?\n")
-#Ask the user for the name of a pet.
-pet_name = input("What is the name of a pet ?\n")
-#Combine the name of their city and pet and show them their band name.
-print("Your band name could be " + pet_name + " " + city)
--- Projects/Day_39/flight-deals/notification_manager.py
+++
@@ -1,3 +0,0 @@
-class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
